aba59a07927e52dd7c76f9bb3afda9082b9f80c8
Furthermore <84783605+wrbyepct@users.noreply.github.com>
2022-10-28 22:16:28 +0800

    Update solution.py

--- solution/solution.py
+++ solution/solution.py
@@ -29,6 +29,45 @@ Approach:
 3. Simple recursion will create too many stacks, instead we can build the 2D array along the way
    So we can directly get the values from precursors, no callbacks needed
 """
+"""
+2022/10/28 updated specs version:
+
+steps_variation(n, s):   { n >= 3 /\ s >= 2 /\ value:N /\ m = n - s}                        # Precondition
+                         if
+                           s = 2 -> value := roundUp((n - 3 + 1) / 2)                       # IFG statement
+                           s > 2 -> m < 3        -> value := 0
+                                    m = (3 \/ 4) -> value := 1
+                                    m = 5        -> value := 2
+                                    m > 5        -> value := steps_variation(m, s) + steps_variation(m, s - 1)
+                         fi           
+                         { \/ value = roundUp((n - 3 + 1) / 2) 
+                           \/ value = 0 
+                           \/ value = 1 
+                           \/ value = 2
+                           \/ value = steps_variation(m, s) + steps_variation(m, s - 1) }   # Post-condition
+                           
+                           
+maxSteps(n): { n >= 3 /\ max_steps = base_steps - 1(loop invariant) }                  # Precondition
+               base_steps := 2                                                         # Initialization
+               max_steps := 1
+             { n > base_steps /\ max_steps = base_steps - 1}                           # Loop condition & Loop invariant
+               do n > steps                                                            # DO statement
+               -> n := n - base_steps,
+                  max_steps := base_steps,
+                  base_steps := base_steps + 1,     
+               od         
+             { n <= base_steps /\ max_steps = base_steps - 1(loop invariant) }         # Post-condition( ~LC /\ LI )
+
+
+total_variation(n, max_base_step): { n >= 3 /\ max_base_step >= 2 }
+                                     i := 2
+                                     total := 1
+                                   { 2 <= i <= max_base_step /\ total = (+ s | 2 <= s <= i : steps_variation(n, s)) }
+                                     do 2 <= i <= max_base_step 
+                                     -> total := total + steps_variation(n, s), 
+                                        i := i + 1 
+                                   { i > max_base_step /\ total = (+ s | 2 <= s <= i : steps_variation(n, s)) }
+"""
 
 
 def get_min_bricks_for_steps():
